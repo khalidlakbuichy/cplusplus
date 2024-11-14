b3f27862e42b018413efb9c0415415f3593978e9
khalidlakbuichy <khalid.lakbuichy@gmail.com>
2024-11-14 09:24:54 +0100

    ex00 done ;)

---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <iostream>
+#include <stdexcept>
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+class Bureaucrat
+{
+    private:
+        const std::string _name;
+        int _grade;
+
+    public:
+        // Canonical form:
+        Bureaucrat();
+        Bureaucrat(const std::string name, int grade);
+        Bureaucrat(const Bureaucrat &copy);
+        Bureaucrat &operator=(const Bureaucrat &op);
+        ~Bureaucrat();
+
+        // Getters:
+        std::string getName() const;
+        int getGrade() const;
+
+        // Methods:
+        void incrementGrade();
+        void decrementGrade();
+
+        // Exceptions:
+        class GradeTooHighException : public std::exception
+        {
+            public:
+                virtual const char *what() const throw();
+        };
+        class GradeTooLowException : public std::exception
+        {
+            public:
+                virtual const char *what() const throw();
+        };
+};
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureaucrat);
+
+#endif
---
+++ cpp05/ex00/main.cpp
@@ -0,0 +1,59 @@
+#include "Bureaucrat.hpp"
+
+int main (void)
+{
+    Bureaucrat b1("b1", 1);
+    Bureaucrat b2("b2", 150);
+    Bureaucrat b3("b3", 75);
+
+    std::cout << b1 << std::endl;
+    std::cout << b2 << std::endl;
+    std::cout << b3 << std::endl;
+
+    // Trying to test the exceptions in the constructor:
+    try
+    {
+        Bureaucrat b4("b4", 0);
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    try
+    {
+        Bureaucrat b5("b5", 151);
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    // Trying to test the exceptions in the incrementGrade and decrementGrade methods:
+    try
+    {
+        Bureaucrat b6("b6", 1);
+        b6.incrementGrade();
+        std::cout << b6 << std::endl;
+        b6.incrementGrade();
+        std::cout << b6 << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    try
+    {
+        Bureaucrat b7("b7", 150);
+        b7.decrementGrade();
+        std::cout << b7 << std::endl;
+        b7.decrementGrade();
+        std::cout << b7 << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return (0);
+}
